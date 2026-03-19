06323101b9b854873d5f9160660db0fb45bb43b4
VCPhuQuoc <139971078+VCPhuQuoc@users.noreply.github.com>
2026-03-19 23:18:25 +0700

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T16:18:04.586Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-19T16:18:24.263Z
+Commit: Fix memory leak in event handlers
