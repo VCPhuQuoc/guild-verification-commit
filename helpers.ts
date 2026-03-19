83ff564e49adb74014001d70a3084540617d386b
VCPhuQuoc <139971078+VCPhuQuoc@users.noreply.github.com>
2026-03-19 23:14:36 +0700

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T16:14:20.823Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-19T16:14:34.404Z
+Commit: Add dark mode support
